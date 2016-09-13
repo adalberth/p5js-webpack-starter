4258a7db02a53b6ca5fc44018c04668596b977f9
David Adalberth Andersen <david@stupid-studio.com>
2016-09-13 10:30:19 +0200

    added alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-    entry: "./index.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
+  entry: "./index.js",
+  output: {
+      path: __dirname,
+      filename: "bundle.js"
+  },
+  resolve: {
+    root: path.resolve(__dirname),
+    alias: {
+      src: 'src',
     },
-    module: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
 		loaders: [
 			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
 		]
